ae3d9ad16e4c758526e401a5b877f5fa0fbb5869
Tony Brown <tony@tonybrown.me>
2016-12-12 12:12:38 -0500

    setup checkbox with an onClick method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import { createStore } from 'redux'
+import ReactDOM             from 'react-dom'
+import React, { Component } from 'react'
+import { createStore }      from 'redux'
 
 const defaultState = { checked: false }
 
@@ -8,17 +8,36 @@ const reducer = function (state = defaultState, action) {
 	switch (action.type) {
 		case 'TOGGLE':
 			return { ...state, checked: !state.checked }
-			break;
 	}
 	return state
 }
 
 const store = createStore(reducer)
 
-class App extends React.Component {
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {}
+	}
+
+	componentWillMount() {
+	  store.subscribe(() => this.setState(store.getState()))
+	}
+
   render() {
+		const onClick = () => store.dispatch({ type: 'TOGGLE' })
     return (
-      <h1>Hello Peeps!</h1>
+		  <div>
+				<h1>To-do's</h1>
+				<div>
+					Learn Redux&nbsp;
+					<input
+						type='checkbox'
+						checked={!!this.state.checked}
+						onClick={onClick}
+					/>
+				</div>
+			</div>
     )
   }
 }
