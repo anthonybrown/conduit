d0208afac1df97e4fa22fb0de0f8c1372bd6f34a
Tony Brown <tony@tonybrown.me>
2016-12-14 17:50:05 -0500

    cleanup state when switching views

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,6 +7,8 @@ export default (state = {}, action) => {
 				inProgress: false,
 				errors: action.error ? action.payload.errors : null
 			}
+		case 'LOGIN_PAGE_UNLOADED':
+			return {}
 		case 'ASYNC_START':
 			if (action.subtype === 'LOGIN' || action.subtype === 'REGISTER') {
 				return { ...state, inProgress: true }
